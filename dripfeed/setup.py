4b7c44a02675566fa876507e8001c2a0e922fe42
Tikitu de Jager <tikitu@logophile.org>
2014-04-19 17:19:39 +0300

    test for run-once

--- dripfeed/setup.py
+++ dripfeed/setup.py
@@ -82,6 +82,7 @@ setup(
     tests_require=[
         'pytest',
         'simplejson',
+        'mock',
     ],
     cmdclass={'test': PyTest}
 )
--- dripfeed/test_dripfeed.py
+++ dripfeed/test_dripfeed.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import shutil
-from dripfeed.comics import Comic
+from dripfeed.comics import Comic, XPathComic
 import os
 import tempfile
 from dripfeed.configs import put_config, Config
+import requests
+import mock
 
 
 def test_put_config_creates_file():
@@ -22,3 +24,26 @@ def test_put_config_creates_file():
         assert '[blah]' in content
     finally:
         shutil.rmtree(d)
+
+
+def first_gunnerkrigg_page():
+    # Stripped-down version of actual page; note the relative URL.
+    result = mock.Mock()
+    result.content = '''
+    <html><head></head>
+    <body>
+      <div class="somewhere">
+        <a href="?p=2"><img src='http://www.gunnerkrigg.com/images/next_a.jpg'></img></a>
+      </div>
+    </body>
+    </html>'''
+    return result
+
+
+def test_run_once():
+    config = Config(comic=XPathComic(name='gunnerkrigg',
+                                     next_xpath="//img[@src='http://www.gunnerkrigg.com/images/next_a.jpg']/.."),
+                    next_url='http://gunnerkrigg.com/?p=1')
+    with mock.patch('requests.get', return_value=first_gunnerkrigg_page()):
+        next_url = config.comic.next_url(config.next_url)
+    assert next_url == 'http://gunnerkrigg.com/?p=2'
